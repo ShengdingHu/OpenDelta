0d5f0c6094eb23b4c0a570aa1e0b09808a35131e
Achazwl <323163497@qq.com>
2022-01-11 18:14:34 +0800

    expandable

--- opendelta/utils/test.py
+++ opendelta/utils/test.py
@@ -1,6 +1,7 @@
 from transformers import T5Model
 from visualization import Visualization
 import web
+import re, copy
 
 model = T5Model.from_pretrained("t5-small")
 vis = Visualization(model)
@@ -41,23 +42,61 @@ def colorfy(label):
     res += '</span>'
     return res
 
-def dfs(o, depth, last, name):
-    html = ""
-    name += o.module_name
-    for i in range(depth-1):
-        html += prefix0 if last[i] else prefix1
-    if depth > 0: html += prefix3 if last[-1] else prefix2
-    length = len(o.children)
-    if length > 0:
-        html += f'<button class="collapsible button_inline">[+]</button>'
-    html += f'<button class="selectable button_inline" name="{name}">{colorfy(o.label)}</button><br/>'
-    html += f'<div class="content">'
-    for i, child in enumerate(o.children):
-        last = last + [i == length-1]
-        html += dfs(child, depth+1, last, name + ".")
-        last.pop()
+compressed_pattern_1 = re.compile("[0-9]+-[0-9]+")
+compressed_pattern_2 = re.compile(".+(,.+)+")
 
-    html += "</div>"
+def expand_part(part):
+    res = []
+    if compressed_pattern_1.fullmatch(part):
+        st, ed = map(int, part.split('-'))
+        for i in range(st, ed+1):
+            res.append( str(i) )
+    elif compressed_pattern_2.fullmatch(part):
+        for c in part.split(','):
+            res.append( c )
+    else:
+        res.append( part )
+    return res
+
+def dfs(o, depth, last, old_name):
+    html = ""
+    module_names = expand_part(o.module_name)
+    if depth > 0:
+        old_last_1 = last[-1]
+    if len(module_names) > 1:
+        module_names = [o.module_name] + module_names
+    for ith, module_name in enumerate(module_names):
+        if ith == 0:
+            html += f'<div>'
+        elif ith == 1:
+            html += f'<div class="expandable-sibling">'
+
+        for i in range(depth-1):
+            html += prefix0 if last[i] else prefix1
+        if depth > 0:
+            last[-1] = old_last_1 & (ith == 0 or ith == len(module_names)-1)
+            html += prefix3 if last[-1] else prefix2
+        length = len(o.children)
+        if length > 0:
+            html += f'<button class="collapsible button_inline">[+]</button>'
+        name = old_name + module_name
+        if ith > 0:
+            label = f'[red]{module_name}{o.label[o.label.index("[",1):]}'
+        else:
+            label = o.label
+        html += f'<button class="selectable button_inline" id="{name}">{colorfy(label)}</button>'
+        if len(module_names) > 1 and ith == 0:
+            html += '<button class="expandable button_inline">[*]</button>'
+        html += '<br/>'
+        html += f'<div class="content">'
+        for i, child in enumerate(o.children):
+            last = last + [i == length-1]
+            html += dfs(child, depth+1, last, name + ".")
+            last.pop()
+
+        html += "</div>"
+        if ith == 0 or (ith > 1 and ith == len(module_names)-1):
+            html += "</div>"
     return html
 
 html = dfs(tree, 0, [], "")
@@ -65,16 +104,10 @@ html = dfs(tree, 0, [], "")
 class hello:
     def GET(self, name):
         return render.test(content=html)
-
 class submit:
-    def expand(self, name):
-        return [name] # TODO implement in visualization?
     def GET(self, _):
-        get_names = web.input().name.split(";")
-        names = []
-        for name in get_names:
-            names += self.expand(name)
-        print(names) # TODO
+        names = web.input().name.split(";")
+        print(names)
 
 if __name__ == "__main__":
     app.run()
